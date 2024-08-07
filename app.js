2fc5bea61510e8cf5b772e0a8f0897ac66353887
Timryan3001 <timryan3001@gmail.com>
2024-08-07 15:05:40 +0100

    adding logic to find worksheets for filtering

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ let vizActiveSheet;
 let dashboard;
 let listSheets;
 
+let saleMap;
+let totalSales;
+let salesByProduct;
+let salesBySegment;
+
 function logWorkbookInformation() {
     workbook = viz.workbook;
     console.log(`The workbook name is : "${workbook.name}"`);
@@ -27,8 +32,30 @@ function logWorkbookInformation() {
         worksheetName = element.name;
         console.log(`The worksheet with index ${index} is: ${worksheetName}`)
     });
+
+    saleMap = listSheets.find((ws) => ws.name == 'SaleMap');
+    totalSales = listSheets.find((ws) => ws.name == 'Total Sales');
+    salesByProduct = listSheets.find((ws) => ws.name == 'SalesbyProduct');
+    salesBySegment = listSheets.find((ws) => ws.name == 'SalesbySegment');
 };
 
+// assingning constants to the buttons
+const oregonWashingtonButton = document.getElementById("oregon_and_washington");
+const clearFilterButton = document.getElementById("clear_filter");
+const undoButton = document.getElementById("undo");
+
+// Functions that tell JS what to do when buttons are clicked
+
+function oregonWashFunction() {
+    //Log what's pressed
+    console.log(oregonWashingtonButton.value);
+
+    //Apply the filter to all of the sheets
+    saleMap.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    totalSales.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesByProduct.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesBySegment.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+}
 
 
 viz.addEventListener("firstinteractive", logWorkbookInformation);
